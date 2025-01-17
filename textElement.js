aa846a4e246a352bf4c5a592838b8951fc7bc342
telecasteren <127958662+telecasteren@users.noreply.github.com>
2025-01-17 16:38:22 +0100

    added text

--- textElement.js
+++ textElement.js
@@ -3,6 +3,9 @@ Kjære Gard,
 Du er en kunstnerisk sjel med et blikk for detaljer - for det vakre og det såre, det fine og det vonde.
 Vi håper du vil fortsette å fange øyeblikkene som kommer, og vise verden slik du ser den.
 
+Gaven din ligger å venter på deg her i Oslo. Vi vet du vil fange mange gode øyeblikk 📷 ! 
+
+
 Glad i deg,
 hilsen Oda, Emilie og Tele
 `;
